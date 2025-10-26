96b9a8cc896f107b8ab6a2ebadd8d4da6ae1c238
leotuor <leotuor@hotmail.com>
2025-10-26 17:00:17 -0300

    feat: botao vincula componente

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -11,12 +11,40 @@
           <ion-title size="large">Task list</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ion-button>Default</ion-button>
+      <ion-button @click="openModal">Add Task</ion-button>
+      <cria-tarefa
+        :is-open="isModalOpen"
+        title="Create New Task"
+        @on-confirm="handleConfirm"
+        @ion-modal-did-dismiss="closeModal"
+      ></cria-tarefa>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import CriaTarefa from '@/components/CriaTarefa.vue';
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalOpen = true;
+    },
+    closeModal() {
+      this.isModalOpen = false;
+    },
+    handleConfirm(taskName: string) {
+      console.log('New task added:', taskName);
+      this.closeModal();
+    },
+  },
+};
 </script>
